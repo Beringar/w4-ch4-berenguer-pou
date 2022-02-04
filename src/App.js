6bee50342c1852e279be47328074827d0ceacb89
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-04 06:55:06 +0100

    added context and contextprovider components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import FormContext from "./components/contexts/FormContext";
 import { useContext } from "react";
 
 function App() {
-  const { incrementStep, decrementStep } = useContext(FormContext);
+  const { formStep, incrementStep, decrementStep, checkForm } =
+    useContext(FormContext);
 
   return (
     <>
@@ -15,24 +16,11 @@ function App() {
             <div className="row g-5">
               <div className="col">
                 <Form />
-                <div className="d-flex justify-content-end">
-                  <Button
-                    className="btn m-2 btn-primary"
-                    actionOnClick={decrementStep}
-                    text="Previous Step"
-                  />
-                  <Button
-                    className="btn m-2 btn-primary"
-                    actionOnClick={incrementStep}
-                    text="Next Step"
-                  />
-                </div>
               </div>
             </div>
           </main>
-
           <footer className="my-5 pt-5 text-muted text-center text-small">
-            <p className="mb-1">&copy; 2022 ISDI Coders</p>
+            <p className="pb-4">&copy; 2022 ISDI Coders</p>
           </footer>
         </div>
       </div>
